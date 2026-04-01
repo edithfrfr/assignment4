1ee304b1ddb618a7475081954818964db518e87f
edithfrfr <edithhernandez03289@gmail.com>
2026-03-31 20:43:53 -0500

    Created expense-service to manage data.Added computed signals and methods to add and delete expenses.

---
+++ src/app/services/expense-service.ts
@@ -0,0 +1,39 @@
+import { computed, signal } from "@angular/core";
+
+class expenseService {
+    //expenses stored as a signal of Expense type
+    expenses = signal<expense[]>([]);
+
+    //categories
+    categories = signal<string[]>(
+        [
+            'Work',
+            'Personl',
+            'Grocery',
+            'Shopping',
+            'Travel',
+            'Food'
+        ]
+    );
+    //computed signals
+    totalExpense = computed(() => 
+    this.expenses().reduce((sum,e) => sum + e.amount, 0));
+
+    highestExpense = computed(() => 
+    this.expenses().length === 0 ? 0 : Math.max( ... this.expenses().map(e => e.amount) ) );
+
+    averageExpense = computed(() =>
+        this.expenses().length === 0 ? 0 : this.totalExpense() / this.expenses().length );
+
+    transactionCount = computed(() => this.expenses().length);
+
+    //methods to add and delete expenses
+    addExpense(expense: expense){
+        this.expenses.update(list => [...list, expense]);
+    }
+
+    deleteExpense(id: string){
+        this.expenses.update(list => list.filter(e => e.id !== id));    
+    }
+
+}
