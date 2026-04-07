b49244dd210b163737d010e0be72a77962c57f25
edithfrfr <edithhernandez03289@gmail.com>
2026-04-07 16:04:26 -0500

    Added ExpenseItemComponent to display a single expense with delete action and style bind.

---
+++ src/app/expense-item/expense-itemComponent.ts
@@ -0,0 +1,34 @@
+import { Component, inject, Input, input } from '@angular/core';
+import { expenseService } from '../services/expense-service';
+
+@Component({
+  selector: 'app-expense-item',
+  imports: [],
+  templateUrl: './expense-item.html',
+  styleUrl: './expense-item.css',
+})
+export class ExpenseItemComponent {
+
+  @Input() expense! : expense;
+
+  private service = inject(expenseService);
+
+  deleteExpense(){
+    this.service.deleteExpense(this.expense.id);
+  }
+
+  editExpense(){
+    //will do this later
+  }
+
+  //style 
+  isHighAmount(){
+    return this.expense.amount >= 100;
+  }
+
+  categoryClass(){
+    return this.expense.category
+
+  }
+
+}
