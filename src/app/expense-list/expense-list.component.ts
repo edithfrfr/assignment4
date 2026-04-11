3079742cbd4bfbcefb296e197b64b6d84d03d6f7
edithfrfr <edithhernandez03289@gmail.com>
2026-04-10 21:03:03 -0500

    Styled view expenses page.

--- src/app/expense-list/expense-list.component.ts
+++ src/app/expense-list/expense-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { expenseService } from '../services/expense-service';
-import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { ExpenseItemComponent } from '../expense-item/expense-item.component';
 
 @Component({
   selector: 'app-expense-list',
   standalone: true,
-  imports: [NgIf, ExpenseItemComponent],
+  imports: [CommonModule,ExpenseItemComponent],
   templateUrl: './expense-list.component.html',
   styleUrl: './expense-list.component.css',
 })
@@ -15,4 +15,8 @@ export class ExpenseListComponent {
   private service = inject(expenseService);
 
   expenses = this.service.expenses;
+
+  deleteExpense(id: string) {
+    this.service.deleteExpense(id);
+  }
 }
